37714f9ade095cf01b76d65fd3b0caf2d752c547
Alejandro Riera <ariera@gmail.com>
2016-05-05 21:04:07 +0200

    fix train dataset generator to not exclude giveaways

--- lib/feature_generation/train.rb
+++ lib/feature_generation/train.rb
@@ -8,18 +8,15 @@ module FeatureGeneration
       %Q{
         SELECT *
         FROM original_train
-        WHERE NOT (productgroup IS NULL
-                   OR quantity < returnquantity
-                   OR voucherid IS NULL)
       }
     end
 
     def self.create_indexes_sql
       %Q{
-        CREATE INDEX idx_#{namespace}_customer  ON #{table_name} (customerid);
-        CREATE INDEX idx_#{namespace}_article   ON #{table_name} (articleid);
-        CREATE INDEX idx_#{namespace}_colorcode ON #{table_name} (colorcode);
-        CREATE INDEX idx_#{namespace}_sizecode  ON #{table_name} (sizecode);
+        CREATE INDEX idx_#{table_name}_customer  ON #{table_name} (customerid);
+        CREATE INDEX idx_#{table_name}_article   ON #{table_name} (articleid);
+        CREATE INDEX idx_#{table_name}_colorcode ON #{table_name} (colorcode);
+        CREATE INDEX idx_#{table_name}_sizecode  ON #{table_name} (sizecode);
       }
     end
   end
